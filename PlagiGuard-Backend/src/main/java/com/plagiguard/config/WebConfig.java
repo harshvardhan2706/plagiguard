d43bea6242c76a49138856f757dbe22724e17119
Harshvardhan Gandharv <hvardhan.1602@gmail.com>
2025-07-31 22:43:54 +0530

    required

--- PlagiGuard-Backend/src/main/java/com/plagiguard/config/WebConfig.java
+++ PlagiGuard-Backend/src/main/java/com/plagiguard/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {    @Override
         registry.addMapping("/**")
                 .allowedOrigins(
                          "https://plagiguard-backend.onrender.com",
+                         "https://plagiguard.onrender.com",
                          "https://plagiguard.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
